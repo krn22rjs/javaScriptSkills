725f2f3714c6cfb50153390432a9d9a1d5e5cf45
Karina Rojas <Karina Rojas>
2016-09-10 18:08:45 -0400

    completed javascript

--- variables/AgeCal/age.js
+++ variables/AgeCal/age.js
@@ -2,7 +2,10 @@
 var year = 2016;
 var birthYear = 1988;
 
-// calculating
+// calculating the age using variables
 
 var age = year - birthYear;
 
+// using console.log for answer to appear
+
+console.log('They are either ' + age + ' or ' + (age-1));
